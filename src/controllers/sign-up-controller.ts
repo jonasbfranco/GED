9fa2c0303cbd1fd740fc8257dfa4bd2c70e724a6
Jonas Baptista Franco <jonas@192.168.0.8>
2026-06-28 23:08:14 -0300

    Controllers-Cases

--- src/controllers/sign-up-controller.ts
+++ src/controllers/sign-up-controller.ts
@@ -1,44 +1,62 @@
 import type { Request, Response } from "express"
 import { z, ZodError } from "zod"
+import { signUpUseCase } from "../use-cases/sign-up-usecases.js"
+
+// import { prisma } from "../lib/prisma/prisma.js"
 
 interface ISignUpRequest{
     email: string,
     password: string
 }
 
-const users: ISignUpRequest[] = []
+//const users: ISignUpRequest[] = []
 const schemaSignUpRequestBody = z.object({
     email: z.email("Email required."),
     password: z.string("Password required.").min(6, "Password must be at least 6 characters."),
 })
 
-export function signUpController(
+export async function signUpController(
     req: Request,
     res: Response
 ){
     try {
         const { email, password } = schemaSignUpRequestBody.parse(req.body)
-    
-        if (!email){ //fazer condição de estar com espaço em branco
-            //throw new Error()
-            return res.status(400).send({ message: "Email required."})
-        }
-    
-        const userExist = users.find(item => item.email == email)
-        if (userExist){
-            //throw new Error()
-            return res.status(409).send({ message: "User already exist."})
-        }
-    
-        users.push({email, password})
+
+        // if (!email){ //fazer condição de estar com espaço em branco
+        //     //throw new Error()
+        //     return res.status(400).send({ message: "Email required."})
+        // }
+
+        // const userExist = await prisma.user.findUnique({
+        //     where: {
+        //         email,
+        //     }
+        // })
+
+        // const userExist = users.find(item => item.email == email)
+        // if (userExist){
+        //     //throw new Error()
+        //     return res.status(409).send({ message: "User already exist."})
+        // }
+
+        //users.push({email, password})
         //console.log(users)
-    
-        return res.status(201).send({ users })
-        
+        // const users = await prisma.user.create({
+        //     data: {
+        //         email,
+        //         password,
+        //     }
+        // })
+
+        const { user } = await signUpUseCase({ email, password })
+
+        // return res.status(201).send({ user })
+        return res.status(201).send()
+
     } catch (error) {
         if(error instanceof ZodError) {
-            return res.status(400).send({ 
-                message: "Validation error", 
+            return res.status(400).send({
+                message: "Validation error",
                 issues: error.format()
             })
         }
---
+++ src/use-cases/sign-up-usecases.ts
@@ -0,0 +1,46 @@
+import { prisma } from "../lib/prisma/prisma.js";
+import { hashSync } from "bcrypt";
+import "dotenv/config";
+
+interface ISignUpUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+interface ISignUpUseCaseResponse {
+  user: {
+    id: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+  };
+}
+
+export async function signUpUseCase({
+  email,
+  password,
+}: ISignUpUseCaseRequest): Promise<ISignUpUseCaseResponse> {
+  const userExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userExist) {
+    throw new Error("User already exist.");
+  }
+
+  const SALT_ROUNDS = Number(process.env.SALT_ROUNDS)
+  const hashPassword = hashSync(password, SALT_ROUNDS)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: hashPassword,
+    },
+  });
+
+  return {
+    user,
+  };
+}
